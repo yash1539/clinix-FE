180a0422f397d779214eaaf179db6f351774f57f
Yash <Yash.singh@internlabs.com>
2023-10-10 13:40:29 +0530

    Added basic structiure

--- src/component/History.js
+++ src/component/History.js
@@ -1,7 +1,45 @@
-import React from "react";
-
+import React, { useState, useEffect } from "react";
+import './history.css'
 function History() {
-  return <div>History</div>;
+  const [patientHistory, setPatientHistory] = useState([]);
+
+  useEffect(() => {
+    // TODO: Fetch patient history data from your API
+    // Replace this with your actual API endpoint
+    fetch("https://api.example.com/patient-history")
+      .then((response) => response.json())
+      .then((data) => setPatientHistory(data))
+      .catch((error) => console.error("Error fetching patient history:", error));
+  }, []); // Run this effect only once on component mount
+
+  return (
+    <div>
+      <h2>Patient History</h2>
+      <table>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Number</th>
+            <th>Latest Visit Date</th>
+            <th>Problem</th>
+            <th>Bill</th>
+          </tr>
+        </thead>
+        <tbody>
+          {patientHistory.map((patient) => (
+            <tr key={patient.pNumber}>
+              <td>{patient.pName}</td>
+              <td>{patient.pNumber}</td>
+              <td>{patient.visits[0]?.cameAt || "N/A"}</td>
+              <td>{patient.visits[0]?.problem || "N/A"}</td>
+              <td>{patient.visits[0]?.pBill || "N/A"}</td>
+              {/* Add an onClick handler to view more details */}
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
 }
 
 export default History;
--- src/component/RegisterPatient.js
+++ src/component/RegisterPatient.js
@@ -1,7 +1,60 @@
-import React from "react";
+import React, { useState } from "react";
 
 function RegisterPatient() {
-  return <div>RegisterPatient</div>;
+  const [patientData, setPatientData] = useState({
+    pName: "",
+    pNumber: "",
+    problem: "",
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setPatientData({ ...patientData, [name]: value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // TODO: Send patientData to your server using API
+    console.log("Patient data submitted:", patientData);
+  };
+
+  return (
+    <div>
+      <h2>Register New Patient</h2>
+      <form onSubmit={handleSubmit}>
+        <label>
+          Patient Name:
+          <input
+            type="text"
+            name="pName"
+            value={patientData.pName}
+            onChange={handleChange}
+          />
+        </label>
+        <br />
+        <label>
+          Phone Number:
+          <input
+            type="text"
+            name="pNumber"
+            value={patientData.pNumber}
+            onChange={handleChange}
+          />
+        </label>
+        <br />
+        <label>
+          Problem:
+          <textarea
+            name="pPrescription"
+            value={patientData.pPrescription}
+            onChange={handleChange}
+          />
+        </label>
+        <br />
+        <button type="submit">Register Patient</button>
+      </form>
+    </div>
+  );
 }
 
 export default RegisterPatient;
